257c6f39ccc1371d0d5130bb36785ab3d41b858e
szianami <szianami@gmail.com>
2022-10-05 23:23:04 +0200

    fix(firebase): sdk version 9

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase"
-// default vs named import
+// import firebase from "firebase"
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { firebaseConfig } from "../firebaseConfig";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,7 +15,5 @@ const app = initializeApp(firebaseConfig);
 
 export const analytics = getAnalytics(app);
 
-export const db = firebase.firestore();
+export const db = getFirestore(app);
 export const auth = getAuth(app);
-
-export default firebase;
